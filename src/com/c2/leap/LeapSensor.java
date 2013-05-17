12acb0b580c5fb3f4f1415dbdb52b08b492fbf1f
Connor Anderson <connora@stanford.edu>
2013-05-17 15:37:49 -0700

    Update LeapSensor to include default methods

--- src/com/c2/leap/LeapSensor.java
+++ src/com/c2/leap/LeapSensor.java
@@ -1,20 +1,47 @@
 package com.c2.leap;
 
-public class LeapSensor {
+import com.leapmotion.leap.Controller;
+import com.leapmotion.leap.Frame;
 
-	/**
-	 * @param args
-	 */
-	
-	
+public class LeapSensor extends Listener {
+
 	private LeapParameterListener listener;
-	
+
 	public void start() {
 		// listener.onLeapParametersChanged(newParameters);
 	}
 
+	@Override
+	public void onInit(Controller controller) {
+		System.out.println("Initialized");
+		System.out.println("Current screen resolution: " + SCREEN_X + "x"
+				+ SCREEN_Y);
+	}
+
+	@Override
+	public void onConnect(Controller controller) {
+		System.out.println("Connected");
+	}
+
+	@Override
+	public void onDisconnect(Controller controller) {
+		System.out.println("Disconnected");
+	}
+
+	@Override
+	public void onExit(Controller controller) {
+		System.out.println("Exited");
+		System.exit(0);
+	}
+
+	@Override
+	public void onFrame(Controller controller) {
+		// Get the most recent frame and report some basic information
+		Frame frame = controller.frame();
+	}
+
 	public void addListener(LeapParameterListener lpl) {
 		listener = lpl;
-		
+
 	}
 }
