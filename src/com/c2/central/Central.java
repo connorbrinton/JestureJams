437909a3246febee36389a2aa565deb7bc4955ee
Connor Brinton <connor.brinton@gmail.com>
2013-05-17 15:30:16 -0700

    Adding SoundGenerator and connecting LeapSensor to SoundGenerator.

--- src/com/c2/central/Central.java
+++ src/com/c2/central/Central.java
@@ -1,13 +1,20 @@
 package com.c2.central;
 
+import com.c2.leap.LeapSensor;
+import com.c2.sound.SoundGenerator;
+
 public class Central {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SoundGenerator sg = new SoundGenerator();
+		sg.start(); // Start sound generator
+		LeapSensor ls = new LeapSensor();
+		ls.start(); // Start leap sensing
+		
+		ls.addListener(sg);// Connect leap sensing to sound generator
 	}
 
 }
---
+++ src/com/c2/sound/SoundGenerator.java
@@ -0,0 +1,19 @@
+package com.c2.sound;
+
+import com.c2.leap.LeapParameterListener;
+import com.c2.leap.LeapParameters;
+
+public class SoundGenerator implements LeapParameterListener {
+
+	@Override
+	public void onLeapParametersChanged(LeapParameters newParameters) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
