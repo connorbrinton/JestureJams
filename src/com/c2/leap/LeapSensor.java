0a79b3cecd337f451fd7be3bf2d559a90a5b931a
Connor Anderson <connora@stanford.edu>
2013-05-27 16:46:50 -0700

    LeapSensor class now contains values for hand position, radius, and finger count. Still need to update LeapParameter.

--- src/com/c2/leap/LeapSensor.java
+++ src/com/c2/leap/LeapSensor.java
@@ -39,9 +39,25 @@ public class LeapSensor extends Listener {
 		// Get the most recent frame and report some basic information
 		Frame frame = controller.frame();
 		
+		// Count Fingers		
+		int fingerCount = frame.fingers().count();
+		
+		HandList hands = frame.hands();
+		
+		// Size of open hand. Ignores outside hand interference to the left.
+		double handSize = hands.rightmost().sphereRadius();
+		
+		// Vector location of hand in x,y, and z coordinates
+		Vector handPosition = hands.rightmost().palmPosition();
+		
+		
+		double positionX = handPosition.getX();
+		double positionY = handPosition.getY();
+		double positionZ = handPosition.getZ();
+		
 		System.out.println("Frame Received");
 		
-	//	listener.onLeapParametersChanged();
+		listener.onLeapParametersChanged(null);
 	}
 
 	public void addListener(LeapParameterListener lpl) {
